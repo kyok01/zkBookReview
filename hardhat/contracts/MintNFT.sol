bd9ec2d1c7207e290a1840229446471f7a434366
yuuta-minede <_jamyukirom@gmail.com>
2023-04-14 23:27:21 +0900

    add transfer function

--- hardhat/contracts/MintNFT.sol
+++ hardhat/contracts/MintNFT.sol
@@ -1,13 +1,18 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MintNFT is ERC721 {
+contract MintNFT is ERC721, Ownable {
     constructor() ERC721("MintNFT", "MintNFT") {}
 
-    function mint(address to, uint256 tokenId) public {
-        _mint(to, tokenId);
+    function mint(address to, uint256 tokenId) public onlyOwner {
+      _mint(to, tokenId);
+    }
+
+    function transfer(address to, uint256 tokenId) public {
+      safeTransferFrom(msg.sender, to, tokenId, "");
     }
 }
 
-// NFT address is 0x9E4E8b56466B77EBaBA538eeFb3f90b52179CEef
+// NFT address is 0xF80D6de6B421a007c2eE3C29AD85e3eA41bfcA65
